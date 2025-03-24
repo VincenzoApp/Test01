c31fb66b7682137530cc3d2b1d63608e773c76b7
VincenzoApp <mttvinz@gmail.com>
2025-03-24 23:18:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,47 @@
-const CACHE_NAME = 'games-cache-v1';
-const urlsToCache = [
+const CACHE_NAME = 'juegos-offline-v1';
+const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
-  '/tris.html',
   '/cuadrado.html',
-  '/manifest.json',
-  'https://cdn.jsdelivr.net/npm/canvas-confetti@1.3.2/dist/confetti.browser.min.js',
-  // Add any other assets like CSS, JS, or images
+  '/tris.html',
+  '/icon.png',
+  '/icon-72x72.png',
+  '/icon-96x96.png',
+  '/icon-128x128.png',
+  '/icon-144x144.png',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+  'https://cdn.jsdelivr.net/npm/canvas-confetti@1.3.2/dist/confetti.browser.min.js'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
       })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
